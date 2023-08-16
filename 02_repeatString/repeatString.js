8da2f823e6d4166520b75ece9100be345afbf91e
Cody Riebel <cody.riebel@gmail.com>
2023-08-16 18:43:54 -0400

    01 to 04 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+  if (times < 0) {
+    return 'ERROR';
+  }
 
+  let rpt = '';
+  for (i = 0; i < times; i++) {
+    rpt += str;
+  }
+  return rpt;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+  for (val of values) {
+    if (array.includes(val)) {
+      const index = array.indexOf(val);
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
