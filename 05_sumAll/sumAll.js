94f6ab2f6dcb82c605651360fd24e859c10f37bc
Cody Riebel <cody.riebel@gmail.com>
2023-08-16 20:53:30 -0400

    completed 05 to 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+  if (
+    firstInt < 0 || 
+    secondInt < 0 ||
+    typeof(firstInt) != 'number' ||
+    typeof(secondInt) != 'number' 
+  ) {
+    return 'ERROR';
+  }
 
+  let sum = 0;
+  if (firstInt > secondInt) {
+    [firstInt, secondInt] = [secondInt, firstInt];
+  }
+    
+  for (let i = firstInt; i <= secondInt; i++) {
+      sum += i;
+    }
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return Number(((5/9) * (f-32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return Number(((9/5) * c + 32).toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
