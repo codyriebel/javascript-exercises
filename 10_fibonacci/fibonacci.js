ca2d2713aa757cb365afd5fa012f57b5432422ba
Cody Riebel <cody.riebel@gmail.com>
2023-09-05 10:50:24 -0400

    completed 10-12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) return 'OOPS';
+  if (n <= 1) return parseInt(n);
+  return fibonacci(n - 2) + fibonacci(n - 1);
 };
+
+// 1, 1, 2, 3
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, current) => {
+    if (!oldest.yearOfDeath) {
+      oldest.yearOfDeath = new Date().getFullYear();
+    }
+    return (oldest.yearOfDeath - oldest.yearOfBirth) > (current.yearOfDeath - current.yearOfBirth) ? oldest : current;
+  });
 };
 
 // Do not edit below this line
